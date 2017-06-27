05cec53368acbacfc2e4028c93ea3e7f2f275d70
Alex Wilmer <djazium@gmail.com>
2017-06-26 20:20:28 -0400

    ✨ Elements, components and HOCs

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ let App
 App = React.createElement('h1', { style: { color: 'violet' } }, 'hi class')
 App = <h1 style={{ color: 'violet' }}>hi class</h1>
 
+// JSX are not strings!
+
+App = "<h1 style={{ color: 'violet' }}>hi class</h1>"
+
 // Element with nested element.
 
 App = React.createElement('h1', null, React.createElement('em', null, 'hello'))
@@ -19,7 +23,7 @@ let name = 'Alex'
 App = React.createElement('h1', null, name)
 App = <h1>{name}</h1>
 
-// Element with list of children
+// Element with array of children
 
 let animals = ['cat', 'dog', 'monkey']
 
@@ -30,6 +34,9 @@ App = React.createElement(
 )
 
 App = <ul>{animals.map(animal => <li key={animal}>{animal}</li>)}</ul>
+
+// PROTIP: Anything that can and will return an array will work!
+// (map, filter, reduce, slice, etc...)
 
 // Conditions!
 
@@ -84,13 +91,69 @@ App = (
 
 // A component that encapsulates an idea
 
-let Game = props =>
+let GameResult = props =>
   <div>
     <h1>{props.win ? 'You win!' : 'You lose!'}</h1>
     <CustomButton primary={props.win}>Play again</CustomButton>
   </div>
 
-App = <Game win={winCoinFlip} />
+App = <GameResult win={winCoinFlip} />
+
+// Components should be designed for maximum reuse
+// often this means passing along props even if you don't use them
+
+// To illustrate this, consider the following situation:
+// Let's pass an onClick handler to our CustomButton
+
+App = (
+  <CustomButton onClick={event => console.log(event)}>Click me!</CustomButton>
+)
+
+// Nothing happens when we click because the `onClick` prop isn't passed along
+// Same thing would happen to other props like `disabled` or `className`
+
+// Instead, let's pass the "rest" of the props to the button
+
+let BetterCustomButton = ({ primary, style, ...props }) =>
+  <button
+    {...props}
+    style={{
+      backgroundColor: primary ? 'chartreuse' : 'tomato',
+      ...style
+    }}
+  />
+
+App = (
+  <BetterCustomButton onClick={event => console.log(event)}>
+    Click me!
+  </BetterCustomButton>
+)
+
+// A higher order component (HOC)
+// A function that takes a component and returns a component
+
+let boldify
+
+boldify = Component => props =>
+  React.createElement(Component, {
+    ...props,
+    style: { fontWeight: 'bold', ...props.style }
+  })
+
+boldify = Component => props =>
+  <Component {...props} style={{ fontWeight: 'bold', ...props.style }} />
+
+let BoldCustomButton = boldify(BetterCustomButton)
+let BoldDiv = boldify(props => <div {...props} />)
+
+App = (
+  <div>
+    <CustomButton primary>Not bold button!</CustomButton>
+    <BoldCustomButton primary>Bold button!</BoldCustomButton>
+    <div>Not bold div!</div>
+    <BoldDiv>Bold div!</BoldDiv>
+  </div>
+)
 
 // Attach our React app to an existing DOM element
 
